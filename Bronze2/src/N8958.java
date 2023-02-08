b7e1e16be3fd4244162043a95b9ada1e174f0787
taek2222 <oot309@daum.net>
2023-02-08 15:42:36 +0900

    BaekJoon

--- Bronze2/src/N8958.java
+++ Bronze2/src/N8958.java
@@ -1,3 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class N8958 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int loop = Integer.parseInt(br.readLine());
+
+        for(int i = 0, total = 0; i < loop; i++) {
+            StringTokenizer str = new StringTokenizer(br.readLine());
+            String[] strarry = str.nextToken().split("");
+            for (int j = 0; j < 10; j++) {
+                //if(strarry[j] == "O")
+                    //total++;
+                System.out.print(strarry[j]);
+            }
+            System.out.println(total);
+            total = 0;
+        }
+    }
 
 }
