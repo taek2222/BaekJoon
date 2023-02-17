ae33e6aa3c7aa21c7875ae078a90780e569115d3
taek2222 <oot309@daum.net>
2023-02-17 15:57:45 +0900

    BaekJoon

--- Bronze5/src/N10871.java
+++ Bronze5/src/N10871.java
@@ -1,2 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class N10871 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] number;
+
+        StringTokenizer AB = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(AB.nextToken());
+        int B = Integer.parseInt(AB.nextToken());
+
+        number = new int[A];
+
+        StringTokenizer numberstr = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < number.length; i++)
+            number[i] = Integer.parseInt(numberstr.nextToken());
+
+        for (int j : number)
+            if (j < B)
+                System.out.print(j + " ");
+    }
 }
