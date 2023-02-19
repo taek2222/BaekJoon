2adf69396c95d5e47dd9194bb8601c2e95971a58
taek2222 <oot309@daum.net>
2023-02-19 16:05:10 +0900

    BaekJoon

--- Bronze3/src/N4153.java
+++ Bronze3/src/N4153.java
@@ -7,19 +7,25 @@ public class N4153 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while(true) {
-            StringTokenizer ABC = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(ABC.nextToken());
-            int B = Integer.parseInt(ABC.nextToken());
-            int C = Integer.parseInt(ABC.nextToken());
+        StringTokenizer ABC = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(ABC.nextToken());
+        int B = Integer.parseInt(ABC.nextToken());
+        int C = Integer.parseInt(ABC.nextToken());
+        while(!(A == 0 && B == 0 && C == 0)) {
 
-            if(A == 0 && B == 0 && C == 0)
-                break;
             if(Math.pow(A, 2) + Math.pow(B, 2) == Math.pow(C, 2))
                 System.out.println("right");
-            else
+            else if(Math.pow(A, 2) + Math.pow(C, 2) == Math.pow(B, 2))
+                System.out.println("right");
+                else if(Math.pow(B, 2) + Math.pow(C, 2) == Math.pow(A, 2))
+                System.out.println("right");
+                else
                 System.out.println("wrong");
 
+            ABC = new StringTokenizer(br.readLine());
+            A = Integer.parseInt(ABC.nextToken());
+            B = Integer.parseInt(ABC.nextToken());
+            C = Integer.parseInt(ABC.nextToken());
         }
     }
 }
