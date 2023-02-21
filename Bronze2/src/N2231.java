e6f88dd0af6a35b4a1eb004e579488e11a66b723
taek2222 <oot309@daum.net>
2023-02-22 00:01:22 +0900

    BaekJoon

--- Bronze2/src/N2231.java
+++ Bronze2/src/N2231.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.stream.Stream;
 
 public class N2231 {
     public static void main(String[] args) throws IOException {
@@ -16,10 +15,16 @@ public class N2231 {
         int constructor = 10000000;
 
         for(int i = number; i >= number-range; i--) {
-            int[] arrnum = Stream.of(String.valueOf(i).split("")).mapToInt(Integer::parseInt).toArray();
+            int[] arrnum = new int[Integer.toString(i).length()];
+            int NUM = i;
+            for(int k = 0; k < arrnum.length; k++) {
+                arrnum[k] = NUM % 10;
+                NUM = NUM / 10;
+            }
+
             int numbercomparison = i;
-            for(int j = 0; j < arrnum.length; j++) {
-                numbercomparison += arrnum[j];
+            for (int k : arrnum) {
+                numbercomparison += k;
             }
             if(numbercomparison == number)
                 if(constructor > i)
