124f7cff285ce9ce76c91ac2c34796f07cefef6b
taek2222 <oot309@daum.net>
2023-02-26 15:18:17 +0900

    BaekJoon

--- Bronze2/src/N15829.java
+++ Bronze2/src/N15829.java
@@ -5,5 +5,6 @@ import java.io.InputStreamReader;
 public class N15829 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     }
 }
