c7926343519bd89112f5c488bba4d3f0c2e7e33b
taek2222 <oot309@daum.net>
2023-03-05 03:33:32 +0900

    BaekJoon

--- Silver5/src/N10814.java
+++ Silver5/src/N10814.java
@@ -8,36 +8,37 @@ public class N10814 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] memberage = new int[Integer.parseInt(br.readLine())];
         String[] membername = new String[memberage.length];
+        StringBuilder str = new StringBuilder();
         String[][] member = new String[memberage.length][2];
 
         for(int i = 0; i < memberage.length; i++) {
             StringTokenizer agename = new StringTokenizer(br.readLine());
-            int age = Integer.parseInt(agename.nextToken());
-            memberage[i] = age;
+            memberage[i] = Integer.parseInt(agename.nextToken());
             membername[i] = agename.nextToken();
         }
 
         for(int i = 0; i < memberage.length; i++) {
             int position = 0;
             for(int j = 0; j < memberage.length; j++) {
-                if(memberage[i] > memberage[j]) {
+                if (memberage[i] > memberage[j]) {
                     position++;
                     continue;
                 }
-                if(memberage[i] == memberage[j]) {
-                    if(membername[i].equals(membername[j]))
-                        continue;
-                    else if(i < j)
-                        position++;
+                if (memberage[i] == memberage[j]) {
+                    if (!(membername[i].equals(membername[j])))
+                        if (i > j)
+                            position++;
                 }
             }
-            member[i][0] = String.valueOf(memberage[i]);
-            member[i][1] = membername[i];
+            member[position][0] = String.valueOf(memberage[i]);
+            member[position][1] = membername[i];
         }
 
         for (int i = 0; i < memberage.length; i++) {
-            System.out.print(member[i][0]);
-            System.out.println(member[i][1]);
+            str.append(member[i][0]).append(" ");
+            str.append(member[i][1]).append("\n");
         }
+
+        System.out.print(str);
     }
 }
