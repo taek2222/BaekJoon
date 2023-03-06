035b25dff4904e1c82072b951e20a3797e28617c
taek2222 <oot309@daum.net>
2023-03-06 14:12:17 +0900

    BaekJoon

--- Silver5/src/N11651.java
+++ Silver5/src/N11651.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class N11650 {
+public class N11651 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[][] number = new int[Integer.parseInt(br.readLine())][2];
@@ -12,14 +12,14 @@ public class N11650 {
 
         for(int i = 0; i < number.length; i++) {
             StringTokenizer num = new StringTokenizer(br.readLine());
-            number[i][0] = Integer.parseInt(num.nextToken());
             number[i][1] = Integer.parseInt(num.nextToken());
+            number[i][0] = Integer.parseInt(num.nextToken());
         }
         Arrays.sort(number, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]);
 
         for (int[] ints : number) {
-            str.append(ints[0]).append(" ");
-            str.append(ints[1]).append("\n");
+            str.append(ints[1]).append(" ");
+            str.append(ints[0]).append("\n");
         }
         System.out.print(str);
     }
