4bf8177e9a8e13670b8b5b919143b05550aada99
taek2222 <oot309@daum.net>
2023-03-07 16:58:09 +0900

    BaekJoon

--- Silver5/src/N11866.java
+++ Silver5/src/N11866.java
@@ -8,23 +8,30 @@ public class N11866 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder str = new StringBuilder();
-        ArrayList<Integer> number = new ArrayList<Integer>();
+        ArrayList<Integer> number = new ArrayList<>();
         StringTokenizer NK = new StringTokenizer(br.readLine());
         int max = Integer.parseInt(NK.nextToken());
         int delete = Integer.parseInt(NK.nextToken()) - 1;
 
         for (int i = 1; i <= max; i++)
             number.add(i);
+        for(int i = delete ; number.size() > 1; ) {
 
-        for(int i = delete; number.size() > 1 ; i += delete) {
-            if( i >= number.size() )
-                i -= number.size();
-            if( i == number.size() )
-                i = 0;
+            System.out.println("현 i : " + i);
             str.append(number.get(i)).append(", ");
-            System.out.println(number.get(i));
             number.remove(i);
+
+            if (i == number.size()) {
+                i = 0;
+            }
+
+            for(int count = 0; count < delete; count++) {
+                    i++;
+                if (i == number.size())
+                    i = 0;
+            }
         }
+
         str.append(number.get(0));
         System.out.println("<"+str+">");
     }
