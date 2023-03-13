7bc32329651e6c1fc9cbd50cdd0966c0fe783158
taek2222 <oot309@daum.net>
2023-03-13 17:14:30 +0900

    BaekJoon

---
+++ Silver4/src/N10828.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class N10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> stack = new ArrayList<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer list = new StringTokenizer(br.readLine());
+            switch (list.nextToken()) {
+                case "push" -> push(Integer.parseInt(list.nextToken()), stack);
+                case "pop" -> pop(stack);
+                case "size" -> size(stack);
+                case "empty" -> empty(stack);
+                case "top" -> top(stack);
+            }
+        }
+    }
+
+    public static void push(int number, ArrayList<Integer> array) {
+        array.add(number);
+    }
+
+    public static void pop(ArrayList<Integer> array) {
+        if(array.size() == 0)
+            System.out.println(-1);
+        else {
+            System.out.println(array.get(array.size() - 1));
+            array.remove(array.size() - 1);
+        }
+    }
+
+    public static void size(ArrayList<Integer> array) {
+        System.out.println(array.size());
+    }
+
+    public static void empty(ArrayList<Integer> array) {
+        if(array.size() == 0)
+            System.out.println(1);
+        else
+            System.out.println(0);
+    }
+
+    public static void top(ArrayList<Integer> array) {
+        if(array.size() == 0)
+            System.out.println(-1);
+        else
+            System.out.println(array.get(array.size()-1));
+    }
+}
