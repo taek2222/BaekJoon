849351b415354185625b18476aa6daf65cae32e9
taek2222 <oot309@daum.net>
2023-03-16 22:30:12 +0900

    BaekJoon

--- Silver4/src/N10816.java
+++ Silver4/src/N10816.java
@@ -6,6 +6,7 @@ import java.util.StringTokenizer;
 
 public class N10816 {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder print = new StringBuilder();
 
