8c9f3022e3078ee0a5eb65fbca95f1402c915f72
taek2222 <oot309@daum.net>
2023-04-10 01:54:20 +0900

    BaekJoon

--- Silver4/src/N10773.java
+++ Silver4/src/N10773.java
@@ -1,16 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class N10773 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] number = new int[Integer.parseInt(br.readLine())];
+        ArrayList<Integer> number = new ArrayList<>();
+        int size = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < number.length; i++)
-            number[i] = Integer.parseInt(br.readLine());
+        for(int i = 0; i < size; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0)
+                number.remove(number.size()-1);
+            else
+                number.add(num);
+        }
 
+        int total = 0;
 
+        for (Integer integer : number) total += integer;
+
+        System.out.println(total);
     }
 }
