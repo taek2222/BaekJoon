5a24ceb7939a5eb43ed95264c068526dcb8b867e
taek2222 <oot309@daum.net>
2023-04-11 03:27:54 +0900

    BaekJoon

--- Silver4/src/N4949.java
+++ Silver4/src/N4949.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.Stack;
 
 public class N4949 {
     public static void main(String[] args) throws IOException {
@@ -8,67 +10,45 @@ public class N4949 {
         StringBuilder bl = new StringBuilder();
 
         String[] str = br.readLine().split("");
-        while(!str[0].equals(".")) {
+        while(!(str[0].equals("."))) {
+            Stack<String> save = new Stack<>();
             int i = 0;
-            while(!(str[i].equals(".")) || !(i == -1) {
-                switch (str[i]) {
-                    case "(":
-                        i = check1(str, i);
-                        i++;
+            while (true) {
+                if(str[i].equals("(") || str[i].equals("["))
+                    save.push(str[i]);
+
+                if(str[i].equals(")")) {
+                    if (save.empty() || !Objects.equals(save.peek(), "(")) {
+                        bl.append("no\n");
                         break;
-                    case "[":
-                        i = check2(str, i);
-                        i++;
-                        break;
-                    case ")", "]":
-                        i = -1;
-                        break;
-                    default:
-                        i++;
+                    }
+                    else
+                        save.pop();
                 }
+
+                if(str[i].equals("]")) {
+                    if (save.empty() || !Objects.equals(save.peek(), "[")) {
+                        bl.append("no\n");
+                        break;
+                    }
+                    else
+                        save.pop();
+                }
+
+                if(str[i].equals("."))
+                    if(save.empty()) {
+                        bl.append("yes\n");
+                        break;
+                    }
+                    else {
+                        bl.append("no\n");
+                        break;
+                    }
+
+                i++;
             }
-            if(i == -1) bl.append("no\n");
-            else bl.append("yes\n");
             str = br.readLine().split("");
         }
         System.out.println(bl);
-    }
-
-    public static int check1(String[] array, int i) {
-        for(; i < array.length; ) {
-            i++;
-            switch (array[i]) {
-                case "(":
-                    i = check1(array, i);
-                    break;
-                case ")":
-                    return i;
-                case "[":
-                    i = check2(array, i);
-                    break;
-                case ".":
-                    return -1;
-            }
-        }
-        return -1;
-    }
-
-    public static int check2(String[] array, int i) {
-        for(; i < array.length; ) {
-            i++;
-            switch (array[i]) {
-                case "[":
-                    i = check2(array, i);
-                    break;
-                case "]":
-                    return i;
-                case "(":
-                    i = check1(array, i);
-                    break;
-                case ".":
-                    return -1;
-            }
-        }
-        return -1;
     }
 }
