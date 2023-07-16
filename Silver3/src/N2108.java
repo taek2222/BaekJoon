554cbf326bf26d7906e9aea16f7c1b0b65ed5963
taek2222 <oot309@daum.net>
2023-07-16 12:59:23 +0900

    BaekJoon

--- Silver3/src/N2108.java
+++ Silver3/src/N2108.java
@@ -21,20 +21,31 @@ public class N2108 {
         int result = (int)Math.round((double)Total / N);
         System.out.println(result); // 산술 평균
 
-        System.out.println(number[(N-1)/2]); // 중앙값
+        System.out.println(number[(N/2)]); // 중앙값
 
-        int[] plus = new int[4000];
-        int[] subtract = new int[4000];
-        for(int i = 0; i < N; i++)
-            if(number[i] >= 0)
-                plus[number[i]]++;
-            else
-                subtract[Math.abs(number[i])]++;
+        int count = 0;
+        int mode_max = 0;
+        int mode = 10000;
+        boolean flag = false;
 
-        for (int i = 0; i < 4000; i++) {
-            System.out.println("+" + plus[i] + "" + i);
-            System.out.println("-" + subtract[i] + "" + i);
-        }
+        for(int i = number[0] + 4000; i <= number[N-1] + 4000; i++)
+
+            if(number[i] > 0) {
+                if(mode_max < number[i]) {
+                    mode_max = number[i];
+                    mode = i - 4000;
+                    flag = true;	// 첫 등장이므로 true 로 변경
+                }
+                // 이전 최빈값 최댓값과 동일한 경우면서 한 번만 중복되는 경우
+                else if(mode_max == number[i] && flag == true) {
+                    mode = i - 4000;
+                    flag = false;
+                }
+            }
+
+        System.out.println(mode);	// 최빈값
+
+        System.out.println(number[N-1] - number[0]); // 범위 출력
 
     }
 }
