52f20aef3783e74774bfa1dd8589fd5055641140
cs <adad0207@naver.com>
2023-07-20 16:39:37 +0900

    BaekJoon

--- Silver3/src/N2108.java
+++ Silver3/src/N2108.java
@@ -17,10 +17,37 @@ public class N2108 {
         }
         Arrays.sort(number);
 
-        for()
+        boolean flag = false;
+        int mode = 0;
+        int max_count = 0;
+
+        for(int i = 0; i < N; ) {
+            int count = 0; // number[i] 에 해당하는 동일한 숫자 계산 변수
+            int skip = 0;
+            int num = number[i];
+
+            for(int j = i; j < N; j++) {
+                if(num != number[j])
+                    break;
+                count++;
+                skip++;
+            }
+
+            if(count > max_count) {
+                max_count = count;
+                mode = num;
+                flag = true;
+            }
+            else if(count == max_count && flag) {
+                mode = num;
+                flag = false;
+            }
+            i += skip;
+        }
 
         System.out.println((int)Math.round((double)Total / N)); // 산술 평균
         System.out.println(number[(N/2)]); // 중앙값
+        System.out.println(mode);
         System.out.println(number[N-1] - number[0]); // 범위 출력
 
     }
